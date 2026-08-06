b74bae0ee5341f42e08e867edc059036e4bb84b8
naiko <3443471283@qq.com>
2026-08-06 12:02:56 +0800

    chore：删除react整改页面全面使用astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [react(),mdx()]
+  integrations: [mdx()]
 });
